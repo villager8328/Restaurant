87533576ebee7133f3aeda0c74621cf554e31d8b
villager8328 <dkeebest@gmail.com>
2026-08-12 16:08:55 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { CartProvider } from './context/CartContext.jsx'
+import { CartProvider } from "./CartContext.jsx";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
